83042a563e413f97915d48ef20bfef82484f7e29
nianman <iman0orujov@gmail.com>
2020-02-23 17:58:57 +0400

    Updates

---
+++ precache-manifest.9cc5017cdcc740a586e7e5be0c1b924e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "99ba4a1a200a8f2dee280024d63ebdc1",
+    "url": "/basic-calculator/index.html"
+  },
+  {
+    "revision": "cb03847f90a79bb45a91",
+    "url": "/basic-calculator/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "542dd91af917fbbe59b0",
+    "url": "/basic-calculator/static/js/2.3e222df5.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/basic-calculator/static/js/2.3e222df5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cb03847f90a79bb45a91",
+    "url": "/basic-calculator/static/js/main.1c1df9a1.chunk.js"
+  },
+  {
+    "revision": "87021b7c862216117599",
+    "url": "/basic-calculator/static/js/runtime-main.b51ceae6.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/basic-calculator/static/media/logo.5d5d9eef.svg"
+  }
+]);
